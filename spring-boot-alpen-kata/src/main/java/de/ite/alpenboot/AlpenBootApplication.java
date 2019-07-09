89a0191ba001b53732137e19b861846a7432fee5
Jan Thewes <jan.thewes@it-economics.de>
2019-07-09 17:18:29 +0200

    Preparations

---
+++ spring-boot-alpen-kata/src/main/java/de/ite/alpenboot/AlpenBootApplication.java
@@ -0,0 +1,12 @@
+package de.ite.alpenboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpenBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlpenBootApplication.class, args);
+    }
+}
